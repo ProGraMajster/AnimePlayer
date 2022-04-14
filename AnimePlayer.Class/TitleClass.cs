f20ee1207d1f19114aa3ec4f046e36de29185010
Kiko kiko <ssasske08@gmail.com>
2022-04-14 18:08:49 +0200

    Update

---
+++ AnimePlayer.Class/TitleClass.cs
@@ -0,0 +1,10 @@
+﻿namespace AnimePlayer.Class
+{
+    public class PreviewTitleClass
+    {
+        public string Title { get; set; }
+        public string LinkToIcon { get; set; } 
+        public List<string> SiteLinks { get; set; }
+        public List<string> LinksToDownloadContent { get; set; }
+    }
+}
