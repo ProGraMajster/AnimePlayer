c5443294b9fad3ca2eebc1aa16884429e5796061
Kiko kiko <ssasske08@gmail.com>
2022-06-07 18:13:45 +0200

    Update

--- AnimePlayer.Class/PreviewTitleClass.cs
+++ AnimePlayer.Class/PreviewTitleClass.cs
@@ -5,5 +5,6 @@
         public string Title { get; set; }
         public List<string> LinkToIcon { get; set; }
         public List<string> LinksToDownloadContent { get; set; }
+        public string S_ID { get; set; }
     }
 }
