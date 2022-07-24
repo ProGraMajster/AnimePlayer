a1c3c0af0a16bd3635d5ec5653e6ff4927193356
Kiko kiko <ssasske08@gmail.com>
2022-07-24 15:28:42 +0200

    Update

--- AnimePlayer/FristRun/FormFristrun.Designer.cs
+++ AnimePlayer/FristRun/FormFristrun.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormFristRun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormFristrun";
+            this.Name = "FormFristRun";
+            this.Text = "FormFristRun";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
