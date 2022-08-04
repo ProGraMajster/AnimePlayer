75283d08847dc1b39fb5a84d290f86c1fb6ba635
Kiko kiko <ssasske08@gmail.com>
2022-08-04 19:19:05 +0200

    Update

--- AnimePlayer.Class/Episode.cs
+++ AnimePlayer.Class/Episode.cs
@@ -12,6 +12,7 @@ namespace AnimePlayer.Class
         public string Title { get; set; }
         public string NumberEpisode { get; set; }
         public string TitleOfEpisode { get; set; }
+        public string Type { get; set; }
         public string AudioLanguage { get; set; }
         public string SubtitleLanguage { get; set; }
         public string LinkToEpisode { get; set; }
