e19e2e2503e74105ebf28ec4b36dccaaf35bd8ea
Kiko kiko <ssasske08@gmail.com>
2023-01-23 20:20:10 +0100

    Update

--- AnimePlayerLib/PanelAllNewsContent.cs
+++ AnimePlayerLib/PanelAllNewsContent.cs
@@ -10,8 +10,6 @@ using System.Windows.Forms;
 using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
-using OpenScraping.Config;
-using OpenScraping;
 
 namespace AnimePlayerLibrary
 {
@@ -29,40 +27,7 @@ namespace AnimePlayerLibrary
 
         private async void buttonLoad_Click(object sender, EventArgs e)
         {
-            if (webView21 != null && webView21.CoreWebView2 != null)
-            {
-                string html = await webView21.ExecuteScriptAsync("document.documentElement.outerHTML;");
-                html = Regex.Unescape(html);
-                html = html.Remove(0, 1);
-                html = html.Remove(html.Length - 1, 1);
-                var configJson = @"
-                {
-                    'img': {
-                        '@class': '',
-                        '@src': 'https://i.imgur.com/RRK1dQv.png',
-                        '@alt': '„Jaku Chara Tomozaki-kun” dostanie nowe anime! image'
-                            },
-                    'p': '',
-                    'div': {
-                        '@class': 'box-more',
-                        'ul': {
-                            '@class': 'inline-tags-numbered'
-                                }
-                        }
-                      }
-            ";
-
-
-                var config = StructuredDataConfig.ParseJsonString(configJson);
-
-
-                var openScraping = new StructuredDataExtractor(config);
-                var scrapingResults = openScraping.Extract(html);
-                foreach (var s in scrapingResults)
-                {
-                    MessageBox.Show(s.ToString());
-                }
-            }
+           
         }
     }
 }
