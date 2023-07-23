660d8d3d903ca79a23442a7ae5e4943be40c3fac
Kiko kiko <ssasske08@gmail.com>
2023-07-23 13:40:11 +0200

    Aktualizacja bibliotek

---
+++ AnimePlayer.Class/CdaEpisode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimePlayer.Class
+{
+    [Serializable]
+    public class CdaEpisode
+    {
+        public Episode Episode { get; set; }
+
+        public string Link360p { get; set; }
+        public string Link480p { get; set; }
+        public string Link720p { get; set; }
+        public string Link1080p { get; set; }
+    }
+}
---
+++ AnimePlayer.Class/People.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimePlayer.Class
+{
+    [Serializable]
+    public class People
+    {
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string DateOfBirth { get; set; }
+    }
+}
